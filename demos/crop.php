9a8afc60b0c1178808b09e625ff3132452958f35
Kelly Hallman <khallman@gmail.com>
2011-06-30 00:28:19 +0000

    Added missing DOCTYPE declaration in demonstration file This problem was causing a rendering bug in MSIE

--- demos/crop.php
+++ demos/crop.php
@@ -27,7 +27,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 // If not a POST request, display page below:
 
-?><html>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 
 		<script src="../js/jquery.min.js"></script>
