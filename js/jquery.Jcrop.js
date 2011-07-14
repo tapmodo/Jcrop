c4d4c268679583b3fa4d2479f6f478b67d17eeab
Phil Bacon <pabacon@gmail.com>
2011-07-14 04:29:17 -0700

    Added call to onSelect from setSelect

--- js/jquery.Jcrop.js
+++ js/jquery.Jcrop.js
@@ -1300,8 +1300,8 @@
     //}}}
     function setSelect(rect) //{{{
     {
-      setSelectRaw([
-      parseInt(rect[0], 10) / xscale, parseInt(rect[1], 10) / yscale, parseInt(rect[2], 10) / xscale, parseInt(rect[3], 10) / yscale]);
+      setSelectRaw([parseInt(rect[0], 10) / xscale, parseInt(rect[1], 10) / yscale, parseInt(rect[2], 10) / xscale, parseInt(rect[3], 10) / yscale]);
+      options.onSelect.call(api, unscale(Coords.getFixed()));
     }
     //}}}
     function setSelectRaw(l) //{{{
