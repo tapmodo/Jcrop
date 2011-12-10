647eaeedc5900c49a52c2f8425b772fc6c32f083
Tapmodo <khallman@tapmodo.com>
2011-12-10 22:39:06 +0000

    Display handles if first selection is made via api

--- js/jquery.Jcrop.js
+++ js/jquery.Jcrop.js
@@ -1440,6 +1440,7 @@
     {
       setSelectRaw([parseInt(rect[0], 10) / xscale, parseInt(rect[1], 10) / yscale, parseInt(rect[2], 10) / xscale, parseInt(rect[3], 10) / yscale]);
       options.onSelect.call(api, unscale(Coords.getFixed()));
+      Selection.enableHandles();
     }
     //}}}
     function setSelectRaw(l) //{{{
