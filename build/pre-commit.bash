06029b6e3e31a675d373139412514e9344c09e34
Tapmodo <khallman@tapmodo.com>
2012-01-09 02:37:57 +0000

    CSS file cleanup - added minimized CSS file Updated related build files

--- build/pre-commit.bash
+++ build/pre-commit.bash
@@ -6,6 +6,14 @@ then
   echo 'No changes made to Jcrop javascript'
 else
   echo 'Building minimized Jcrop javascript file'
-  bash build/minimize.bash
+  bash build/minimize-js.bash
+fi
+
+if git diff --no-ext-diff --cached --exit-code css/jquery.Jcrop.css > /dev/null
+then
+  echo 'No changes made to Jcrop CSS file'
+else
+  echo 'Building minimized Jcrop CSS file'
+  bash build/minimize-css.bash
 fi
 
