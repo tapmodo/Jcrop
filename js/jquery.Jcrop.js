d8b7b4a7be8363b10065ba343f1af1a6e164f564
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2012-02-17 10:04:10 +0100

    Allow bypassing of setSelect callback when using the API

--- js/jquery.Jcrop.js
+++ js/jquery.Jcrop.js
@@ -1374,10 +1374,12 @@
       queueAnimator();
     }
     //}}}
-    function setSelect(rect) //{{{
+    function setSelect(rect, opts) //{{{
     {
       setSelectRaw([parseInt(rect[0], 10) / xscale, parseInt(rect[1], 10) / yscale, parseInt(rect[2], 10) / xscale, parseInt(rect[3], 10) / yscale]);
-      options.onSelect.call(api, unscale(Coords.getFixed()));
+      if (!(opts && opts['silent'])) {
+        options.onSelect.call(api, unscale(Coords.getFixed()));
+      }
       Selection.enableHandles();
     }
     //}}}
