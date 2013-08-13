1893ba9e449115dde236ec6933cdfb3fcf3c9c3e
Tapmodo <khallman@tapmodo.com>
2013-08-13 21:34:28 +0000

    Added ImageLoader component

--- js/Jcrop.js
+++ js/Jcrop.js
@@ -954,6 +954,42 @@
     //}}}
   });
   // }}}
+  var ImageLoader = function(src,element,cb){
+    this.src = src;
+    if (!element) element = new Image;
+    this.element = element;
+    this.callback = cb;
+    this.load();
+  };
+  $.extend(ImageLoader.prototype,{
+    getDimensions: function(){
+      var el = this.element;
+
+      if (el.naturalWidth)
+        return [ el.naturalWidth, el. naturalHeight ];
+
+      if (el.width)
+        return [ el.width, el.height ];
+
+      return null;
+    },
+    fireCallback: function(){
+      if (typeof this.callback == 'function')
+        this.callback.apply(this,this.getDimensions());
+    },
+    isLoaded: function(){
+      return this.element.complete;
+    },
+    load: function(){
+      var t = this;
+      var el = t.element;
+
+      el.src = t.src;
+
+      if (t.isLoaded()) this.fireCallback();
+        else this.element.onload = function(e){ t.fireCallback(); };
+    }
+  });
 
   /**
    *  Jcrop
@@ -998,6 +1034,7 @@
     //}}}
     //component: internal components {{{
     component: {
+      ImageLoader: ImageLoader,
       DragState: DragState,
       StageManager: StageManager,
       Animator: CropAnimator,
