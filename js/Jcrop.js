470abba350f10dbd6461e08db97cd01e7f73f8b8
Tapmodo <khallman@tapmodo.com>
2013-08-16 21:58:20 +0000

    Added experimental grid filter

--- js/Jcrop.js
+++ js/Jcrop.js
@@ -164,6 +164,35 @@
     }
   });
   // }}}
+  // GridFilter {{{
+  /**
+   *  GridFilter
+   *  a filter to constrain crop selection to bounding element
+   *  This filter simply rounds coordinate values to integers
+   */
+  var GridFilter = function(){
+    this.stepx = 1;
+    this.stepy = 1;
+    this.core = null;
+  };
+  $.extend(GridFilter.prototype,{
+    priority: 19,
+    filter: function(b){
+      
+      var n = {
+        x: Math.round(b.x / this.stepx) * this.stepx,
+        y: Math.round(b.y / this.stepy) * this.stepy,
+        x2: Math.round(b.x2 / this.stepx) * this.stepx,
+        y2: Math.round(b.y2 / this.stepy) * this.stepy
+      };
+      
+      n.w = n.x2 - n.x;
+      n.h = n.y2 - n.y;
+
+      return n;
+    }
+  });
+  // }}}
   // ConstrainFilter {{{
   /**
    *  ConstrainFilter
