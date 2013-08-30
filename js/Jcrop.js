035de9d7dd6d2195bebe24002b3e71afada68d38
Tapmodo <khallman@tapmodo.com>
2013-08-30 16:35:31 +0000

    Implemented multiCleanup feature

--- js/Jcrop.js
+++ js/Jcrop.js
@@ -666,20 +666,38 @@
 
   $.extend(StageDrag.prototype,{
     start: function(e){
-      if (!this.core.opt.allowSelect) return false;
-      if (this.core.opt.multiMax && (this.core.ui.multi.length >= this.core.opt.multiMax)) return false;
+      var c = this.core;
 
+      // Do nothing if allowSelect is off
+      if (!c.opt.allowSelect) return false;
+
+      // Also do nothing if we can't draw any more selections
+      if (c.opt.multi && c.opt.multiMax && (c.ui.multi.length >= c.opt.multiMax)) return false;
+
+      // calculate a few variables for this drag operation
       var o = $(e.currentTarget).offset();
       var origx = e.pageX - o.left + this.offset[0];
       var origy = e.pageY - o.top + this.offset[1];
-      var m = this.core.ui.multi;
+      var m = c.ui.multi;
 
-      if (!this.core.opt.multi) {
-        for(var i=0;i<m.length;i++) m[i].remove();
-        this.core.ui.multi = [];
+      // Determinenewly dragged crop behavior if multi disabled
+      if (!c.opt.multi) {
+        // For multiCleaanup true, remove all existing selections
+        if (c.opt.multiCleanup){
+          for(var i=0;i<m.length;i++) m[i].remove();
+          c.ui.multi = [];
+        }
+        // If not, only remove the currently active selection
+        else {
+          c.removeSelection(c.ui.selection);
+        }
       }
 
-      var sel = this.core.newSelection().update(Jcrop.wrapFromXywh([origx,origy,1,1]));
+      // Create the new selection
+      var sel = c.newSelection()
+        // and position it
+        .update(Jcrop.wrapFromXywh([origx,origy,1,1]));
+      
       return sel.startDrag(e,'se');
     },
     end: function(x,y){
@@ -1160,6 +1178,7 @@
       allowSelect: true,
       multi: false,
       multiMax: false,
+      multiCleanup: true,
       animation: true,
       animEasing: 'swing',
       animDuration: 400,
