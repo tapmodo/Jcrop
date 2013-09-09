9b55c38501e81524a285082b2fa4aad30c7e1f66
Tapmodo <khallman@tapmodo.com>
2013-09-09 15:32:44 +0000

    Updated KeyManager code

--- src/component/KeyWatcher.js
+++ src/component/KeyWatcher.js
@@ -8,54 +8,56 @@
     this.init();
   };
   // }}}
-  // KeyWatcher.defaults = {{{
-  KeyWatcher.defaults = {
-    passthru: [ 9 ],
-    debug: false
-  };
-  // }}}
-
-  $.extend(KeyWatcher.prototype,{
-    // init: function(){{{
-    init: function(){
-      $.extend(this,KeyWatcher.defaults);
-      this.enable();
+  $.extend(KeyWatcher,{
+    // defaults: {{{
+    defaults: {
+      eventName: 'keydown.jcrop',
+      passthru: [ 9 ],
+      debug: false
     },
     // }}}
-    // disable: function(){{{
-    disable: function(){
-      this.core.container.off('keydown.jcrop');
-    },
-    // }}}
-    // enable: function(){{{
-    enable: function(){
-      var t = this, m = t.core;
-      m.container.on('keydown.jcrop',function(e){
-        var nudge = e.shiftKey? 16: 2;
+    prototype: {
+      // init: function(){{{
+      init: function(){
+        $.extend(this,KeyWatcher.defaults);
+        this.enable();
+      },
+      // }}}
+      // disable: function(){{{
+      disable: function(){
+        this.core.container.off(this.eventName);
+      },
+      // }}}
+      // enable: function(){{{
+      enable: function(){
+        var t = this, m = t.core;
+        m.container.on(t.eventName,function(e){
+          var nudge = e.shiftKey? 16: 2;
 
-        if ($.inArray(e.keyCode,t.passthru) >= 0)
-          return true;
+          if ($.inArray(e.keyCode,t.passthru) >= 0)
+            return true;
 
-        switch(e.keyCode){
-          case 37: m.nudge(-nudge,0); break;
-          case 38: m.nudge(0,-nudge); break;
-          case 39: m.nudge(nudge,0); break;
-          case 40: m.nudge(0,nudge); break;
+          switch(e.keyCode){
+            case 37: m.nudge(-nudge,0); break;
+            case 38: m.nudge(0,-nudge); break;
+            case 39: m.nudge(nudge,0); break;
+            case 40: m.nudge(0,nudge); break;
 
-          case 46:
-          case 8:
-            m.requestDelete();
-            return false;
-            break;
+            case 46:
+            case 8:
+              m.requestDelete();
+              return false;
+              break;
 
-          default:
-            if (t.debug) console.log('keycode: ' + e.keyCode);
-            break;
-        }
+            default:
+              if (t.debug) console.log('keycode: ' + e.keyCode);
+              break;
+          }
 
-        if (!e.metaKey && !e.ctrlKey)
-          e.preventDefault();
-      });
+          if (!e.metaKey && !e.ctrlKey)
+            e.preventDefault();
+        });
+      }
+      // }}}
     }
-    // }}}
   });
