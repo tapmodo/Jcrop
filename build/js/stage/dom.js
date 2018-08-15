8b5e4b6b210f2fe6d203b725ae37499e2ec09f98
Kelly Hallman <khallman@tapmodo.com>
2018-08-15 01:00:39 -0700

    Bugfix; check if this.shades is defined

--- build/js/stage/dom.js
+++ build/js/stage/dom.js
@@ -22,8 +22,8 @@ class Stage extends ConfObj {
   initOptions(){
     this._optconf['multi'] = v => { if (!v) this.limitWidgets() };
     this._optconf['shading'] = v => this.updateShades();
-    this._optconf['shadeColor'] = v => this.shades.setStyle(v);
-    this._optconf['shadeOpacity'] = v => this.shades.setStyle(null,v);
+    this._optconf['shadeColor'] = v => this.shades && this.shades.setStyle(v);
+    this._optconf['shadeOpacity'] = v => this.shades && this.shades.setStyle(null,v);
   }
 
   limitWidgets(n=1) {
