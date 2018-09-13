650339f30a59ce09bea44192457a2762639ecef8
Kelly Hallman <khallman@tapmodo.com>
2018-09-13 16:30:08 -0700

    Added eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  env: {
+    browser: true,
+  },
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow no space after comma
+    'comma-spacing': 0,
+    'space-infix-ops': 0,
+    // allow extra semi-colons
+    'semi': [ 'error', 'always' ],
+    // trailing comma
+    'comma-dangle': 1
+  }
+}
